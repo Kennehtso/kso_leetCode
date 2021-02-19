fb5308fd23cbee645aedba37b1979d479932bb82
Kenneth So <kennethso1211@gmail.com>
2021-02-19 19:55:26 +0800

    #283, Single-line

---
+++ Problems/_1_Easy/_283.py
@@ -0,0 +1,35 @@
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c = nums.count(0)
+        while c > 0:
+            i = nums.index(0)
+            print(f"i: {i}")
+            nums.pop(i)
+            nums.append(0)
+            c-=1
+        print(f"nums: {nums}")
+        #print(nums)
+        
+slt = Solution()
+# Test Case
+#result = slt.longestCommonPrefix(["dog","racecar","car"])
+slt.moveZeroes([0,1,0,3,12])
+
+
+"""
+283. Move Zeroes
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+    Input: [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+
+Note:
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+"""
