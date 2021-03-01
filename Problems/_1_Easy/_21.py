c867b28330d458ff467b39331b7a2d6b50aec734
Kenneth So <kennethso1211@gmail.com>
2021-03-01 18:35:32 +0800

    #21, rev pass, 97%

--- Problems/_1_Easy/_21.py
+++ Problems/_1_Easy/_21.py
@@ -1,56 +1,74 @@
 from typing import List
+import copy
 class ListNode:
     def __init__(self, val=0, next =None):
         self.val = val
         self.next = next
 
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode, round : int, nodeAsArr: []) -> ListNode:
-        round+=1
-        print(f"Round {round}: ")
-        print(f"nodeAsArr: {nodeAsArr}")
-        if l1 is None:
-            print(f"l1 is None, return l2")
-            print("------------------------------------")
-            nodeAsArr.append(l2.val)
-            print(f"return l2: {l2.val}")
-
-            return l2
-        elif l2 is None:
-            print(f"l2 is None, return l1")
-            print("------------------------------------")
-            nodeAsArr.append(l1.val)
-            print(f"return l1: {l1.val}")
-
-            return l1
-        elif l1.val < l2.val:
-            
-            l1_dis = None if l1.next is None else l1.next.val
-            l2_dis = None if l2 is None else l2.val
-            print(f"l2 is Higher, return mergeTwoLists(l1 next: {l1_dis}, l2 :{l2_dis})")
-            print("------------------------------------")
-            nodeAsArr.append(l1.val)
-
-            l1.next = self.mergeTwoLists(l1.next, l2, round, nodeAsArr)
-            print(f"return l1: {l1.val}")
-            return l1
-        else:
-            l1_dis = None if l1 is None else l1.val
-            l2_dis = None if l2.next is None else l2.next.val
-            print(f"l2 is Lower, return mergeTwoLists(l1 : {l1_dis}, l2 next:{l2_dis})")
-            print("------------------------------------")
-            nodeAsArr.append(l2.val)
-
-            l2.next = self.mergeTwoLists(l1, l2.next, round, nodeAsArr)
-            print(f"return l2: {l2.val}")
-            return l2
+     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None and l2 is None: return None
+        elif l1 is None and l2: return l2
+        elif l1 and l2 is None: return l1
+        work = result = ListNode(0)
+        cur_l1, cur_l2 = l1, l2
+        while cur_l1 or cur_l2  :
+            if cur_l1 is None:
+                work.next = cur_l2
+                cur_l2 = cur_l2.next
+                #print(f"cur_l1 is None:")
+            elif cur_l2 is None:
+                work.next = cur_l1
+                cur_l1 = cur_l1.next
+                #print(f"cur_l2 is None")
+                
+            elif cur_l1.val > cur_l2.val :
+                work.next = cur_l2
+                cur_l2 = cur_l2.next
+                #print(f"cur_l1.val > cur_l2.val")
+               
+            else: # cur_l1.val <= cur_l2.val
+                work.next = cur_l1
+                cur_l1 = cur_l1.next
+                #print(f"cur_l1.val <= cur_l2.val")
+            """   
+            print(f"Work: {work}")
+            print("---------------------------")
+            print(f"cur_l1: {cur_l1}")
+            print("---------------------------")
+            print(f"cur_l2: {cur_l2}")
+            print("----------------------------------------------------")
+            """ 
+            work = work.next
+        
+        """
+        print(f"--- The End ---")
+        print(f"result: {result}")
+        print("---------------------------")
+        print(f"Work: {work}")
+        print("---------------------------")
+        print(f"cur_l1: {cur_l1}")
+        print("---------------------------")
+        print(f"cur_l2: {cur_l2}")
+        print("----------------------------------------------------")
+        """
+        """
+        # print result
+        chk, chkList = copy.copy(result), []
+        while chk:
+            chkList.append(chk.val) 
+            chk = chk.next
+        chkList.pop(0)
+        print(chkList)
+        """
+        return result.next
 
 slt = Solution()
 
 # Test Case
 l1 = ListNode(1, ListNode(2, ListNode(3,  ListNode( 5,  ListNode(8, None)))))
 l2 = ListNode(1, ListNode(3, ListNode(4,  ListNode( 5,  ListNode(6, ListNode(7, None))))))
-slt.mergeTwoLists(l1,l2, 0, [])
+slt.mergeTwoLists(l1,l2)
 """
 21. Merge Two Sorted Lists
 Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
